c5fad291a8da999dbeb3546054aa88f3fbec9b85
Jonathan Jaffe <jjaffe@thirdrating.com>
2014-05-20 22:48:36 +0000

    added uglify to compress size, updates to js

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,6 +8,7 @@ module.exports = function(grunt) {
     //var pkg = require("./package.json");
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     
 
     grunt.initConfig({
@@ -18,12 +19,28 @@ module.exports = function(grunt) {
             options: {
                 jshintrc: '.jshintrc'
             }
-        }
+        },
+        
+          uglify: {
+            options: {
+              mangle: true,
+              compress: true,
+              banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+                '<%= grunt.template.today("yyyy-mm-dd") %> */'
+            },
+            build: {
+              files: {
+                './thirdrating-jquery.min.js': ['js/thirdrating-jquery.js'],
+                './thirdrating-jquery.min.css': ['css/thirdrating-jquery.css']
+              }
+            }
+          }
+        
     });
 
     // By default, check syntax
     
-    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('default', ['jshint', 'uglify:build']);
 
 
 };
--- js/thirdrating-jquery.js
+++ js/thirdrating-jquery.js
@@ -1,3 +1,5 @@
+/*global window jQuery  */
+
 /**
  * Url: https://github.com/jjaffe/thirdrating-jquery
  * Author: Jonathan Jaffe
@@ -14,7 +16,7 @@
     // constants
     var CONST = {
         
-        thirdRatingUrl: "http://the.thirdrating.com/api"
+        thirdRatingUrl: "http://the.thirdrating.com/api/v1/"
         
     };
     
@@ -48,6 +50,23 @@
             // process returned data on success
             .done(function (data) {
                 
+                // ok, our API will guarantee a return order in the same order
+                // do quick confirm
+                
+                if (data.marks.length !== marks.length) {
+                    return plugin.settings.onError;
+                }
+                
+                // loop through our results
+                for (var m = 0; m < data.marks.length; m++) {
+                    
+                    // call our handler
+                    onComplete(m, data.marks[m]);
+                    
+                }
+                
+                // fire event
+                plugin.settings.afterRefresh();
                 
                 
             })
@@ -75,6 +94,65 @@
         
         // call our refresh - returns indexed data
         refreshMarks(marks, function onComplete(index, data) {
+            
+            /** 
+             * data returned will be in the form:
+             * 
+             * {
+             * 
+             *      mark: 'name of the mark',
+             *      markUrl: 'hyperlink to the mark',
+             *      lastRating: nnn,
+             *      change: +-1-3 or 0 if neutral rating,
+             *      by: {
+             *          name: 'Actual Name',
+             *          url: 'link to name'
+             *      },
+             *      on: dateTime UTC format,
+             *      note: 'short note of up to 140 characters'
+             * 
+             * }
+             * 
+             **/
+            
+            var elem = elems.eq(index);
+            
+            // first step, empty the element
+            elem.empty();
+            
+            // construct the mark
+            var spanMark = $('span.mark');
+            
+            var markLink = $('a');
+            markLink.text(data.mark);
+            markLink.attr('href', data.markUrl);
+            
+            spanMark.append(markLink);
+            
+            // construct the rating
+            var rating = $('span.last-rating');
+            rating.text(data.lastRating);
+            
+            // construct the trsymbol
+            var trSymbol = $('span.tr-symbol');
+            trSymbol.append($('span.t'))
+                .append($('span.m'))
+                .append($('span.b'));
+            
+            // construct the element
+            elem.append(spanMark)
+                .append(rating)
+                .append(trSymbol);
+                
+            // show change if specified in params
+            if (elem.data('showLastChange') || plugin.settings.showLastChange) {
+                
+                var lastChange = $('span.last-change');
+                lastChange.text(data.lastChange);
+                elem.append(lastChange);
+                
+            }
+            
             
             
             
